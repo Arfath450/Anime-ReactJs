9dce1c49716f7099666705198b58777644f2d2fc
Arfath450 <46299355+Arfath450@users.noreply.github.com>
2020-06-18 10:58:21 +0530

    Updates

---
+++ precache-manifest.a0d9e41e826dd0a5049b323321e3d2e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef40c49adc969d30ac1e79dedb323960",
+    "url": "/Anime-ReactJs/index.html"
+  },
+  {
+    "revision": "e175e0fc7e1e2cf970fe",
+    "url": "/Anime-ReactJs/static/css/main.996e5b80.chunk.css"
+  },
+  {
+    "revision": "93e832caf6fdeae2a4f2",
+    "url": "/Anime-ReactJs/static/js/2.7c129213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Anime-ReactJs/static/js/2.7c129213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e175e0fc7e1e2cf970fe",
+    "url": "/Anime-ReactJs/static/js/main.8a07ec4e.chunk.js"
+  },
+  {
+    "revision": "2f08ef2fbcb346fdba4b",
+    "url": "/Anime-ReactJs/static/js/runtime-main.262b8f46.js"
+  }
+]);
